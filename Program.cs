ff247d622acbae7d46d468eaa59fab8d163ff8bd
chris-wamae <chriswamae123@gmail.com>
2023-06-15 10:00:36 +0300

    Refactored User choice switch statement to a dictionary and created method for collecting measurements for figures

--- Program.cs
+++ Program.cs
@@ -10,7 +10,88 @@
 // Console.WriteLine(Areas.Centimetres(10000,"m"));
 // Console.WriteLine(Areas.Millimetres(10000,"cm"));
 // Console.WriteLine(Volumes.Kilometres(1,"m"));
-int calulation_choice = General.Calculation_message();
-int shape_choice = General.Shape_message(calulation_choice);
+// int calulation_choice = General.Calculation_message();
+// int shape_choice = General.Shape_message(calulation_choice);
+// int[] choices = {calulation_choice, shape_choice};
 
+// switch(choices){
+//  case [1,1] :
+//  Console.WriteLine("Hey");
+//  break;
+//   case [1,2] :
+//  Console.WriteLine("tyt");
+//  break;
+//   case [1,3] :
+//  Console.WriteLine("dade");
+//  break;
+//   case [1,4] :
+//  Console.WriteLine("dadeda");
+//  break;
+//   case [1,5] :
+//  Console.WriteLine("fvsvv");
+//  break;
+//   case [2,1] :
+//  Console.WriteLine("akja");
+//  break;
+//   case [2,2] :
+//  Console.WriteLine("oisrskj");
+//  break;
+//   case [2,3] :
+//  Console.WriteLine("opaiekjeakj");
+//  break;
+//   case [2,4] :
+//  Console.WriteLine("mccjccj");
+//  break;
+//   case [2,5] :
+//  Console.WriteLine("oeoepe");
+//  break;
+// case [3,1] :
+//  Console.WriteLine("daEED");
+//  break;
+//   case [3,2] :
+//  Console.WriteLine("reueuru");
+//  break;
+//   case [3,3] :
+//  Console.WriteLine("Heydede");
+//  break;
+//  case [3,4] :
+//  Console.WriteLine("Heykfkfkfkfk");
+//  break;
+//   case [3,5] :
+//  Console.WriteLine("Heydeffe");
+//  break;
+// }
+
+General.Units_message();
+
+// Dictionary<(int, int), Action> choiceActions = new Dictionary<(int, int), Action>
+// {
+//     {(1, 1), () => Console.WriteLine("Hey")},
+//     {(1, 2), () => Console.WriteLine("tyt")},
+//     {(1, 3), () => Console.WriteLine("dade")},
+//     {(1, 4), () => Console.WriteLine("dadeda")},
+//     {(1, 5), () => Console.WriteLine("fvsvv")},
+//     {(2, 1), () => Console.WriteLine("akja")},
+//     {(2, 2), () => Console.WriteLine("oisrskj")},
+//     {(2, 3), () => Console.WriteLine("opaiekjeakj")},
+//     {(2, 4), () => Console.WriteLine("mccjccj")},
+//     {(2, 5), () => Console.WriteLine("oeoepe")},
+//     {(3, 1), () => Console.WriteLine("daEED")},
+//     {(3, 2), () => Console.WriteLine("reueuru")},
+//     {(3, 3), () => Console.WriteLine("Heydede")},
+//     {(3, 4), () => Console.WriteLine("Heykfkfkfkfk")},
+//     {(3, 5), () => Console.WriteLine("Heydeffe")}
+// };
+
+// int calculationChoice = General.Calculation_message();
+// int shapeChoice = General.Shape_message(calculationChoice);
+
+// if (choiceActions.TryGetValue((calculationChoice, shapeChoice), out Action action))
+// {
+//     action.Invoke();
+// }
+// else
+// {
+//     Console.WriteLine("Invalid choice");
+// }
 
--- messages/General.cs
+++ messages/General.cs
@@ -3,13 +3,21 @@ public class General
     public static int Calculation_message()
     {
         Console.WriteLine("Hi!");
+
         Console.WriteLine("What would you like to calculate?");
+
         Console.WriteLine("1.Perimeter");
+
         Console.WriteLine("2.Area");
+
         Console.WriteLine("3.Volume");
+
         Console.WriteLine("Enter a number and hit enter to continue...");
+
         int user_choice;
+
         user_choice = Convert.ToInt32(Console.ReadLine());
+
         return user_choice;
     }
 
@@ -28,7 +36,7 @@ public class General
                 choice = "volume";
                 break;
         }
-        
+
         Console.WriteLine($"What would you like to calculate the {choice} for?");
 
         String[] shapes = { "Square", "Rectangle", "Triangle", "Trapezium", "Circle" };
@@ -41,7 +49,55 @@ public class General
         }
 
         int shape_choice = Convert.ToInt32(Console.ReadLine());
+
         return shape_choice;
     }
 
+    public static void Units_message()
+    {
+     Action find_measurement = () => {
+        Console.WriteLine("What are the measurements?");
+
+        Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("In what unit is this measurement?");
+
+        Console.WriteLine("1.Kilometres");
+
+        Console.WriteLine("2.Metres");
+
+        Console.WriteLine("3.Centimetres");
+
+        Console.WriteLine("4.Millimetres");
+
+        Convert.ToInt32(Console.ReadLine());
+
+     };
+     find_measurement.Invoke();
+
+        
+
+        bool other_measurement = true;
+
+        while (other_measurement == true)
+        {
+            Console.WriteLine("Is there any other measurement?");
+
+            Console.WriteLine("1.Yes");
+
+            Console.WriteLine("2.No");
+            int new_measurement = Convert.ToInt32(Console.ReadLine());
+
+            if (new_measurement == 1)
+            {
+                find_measurement.Invoke();
+            }
+            else
+            {
+                other_measurement = false;
+            }
+
+
+        }
+    }
 }
