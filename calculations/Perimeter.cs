0a57fe7e2c5fa59e0ae958c0faee4f80e8ec7e5b
chris-wamae <chriswamae123@gmail.com>
2023-06-19 07:23:28 +0300

    Refactored perimeter methods toaccept an array as parameters

--- calculations/Perimeter.cs
+++ calculations/Perimeter.cs
@@ -1,34 +1,45 @@
 public class Perimeter
 {
-    public static double Square(double side)
+    public static double Square(params double[] sides)
     {
-        return side * 4;
+        return sides[0] * 4;
     }
 
-    public static double Rectangle(double length, double width)
-    {
+    public static double Rectangle(params double[] sides)
+    {   
+        double length = sides[0];
+        double width = sides[1];
+       
         return 2 * (length + width);
     }
 
-    public static double Triangle(double side1, double side2, double side3)
+    public static double Triangle(params double[] sides)
     {
-        return side1 + side2 + side3;
+        return sides[0] + sides[1] + sides[2];
     }
 
-    public static double Trapezium(double slant_side, double top, double bottom)
-    {
+    public static double Trapezium(params double[] sides)
+    {   
+        double slant_side = sides[0];
+        double top = sides[1];
+        double bottom = sides[2];
+
         return (slant_side * 2) + (top + bottom);
     }
 
-    public static double Trapezium_straight(double slant_side, double top, double bottom, double straight_side)
-    {
+    public static double Trapezium_straight(params double[] sides)
+    {  
+        double slant_side = sides[0];
+        double top = sides[1];
+        double bottom = sides[2];
+        double straight_side = sides[3];
 
         return slant_side + top + bottom + straight_side;
     }
 
-    public static double Circle(double radius)
+    public static double Circle(params double[] radius)
     {
-        return Math.PI * (radius * 2);
+        return Math.PI * (radius[0] * 2);
     }
 
     
