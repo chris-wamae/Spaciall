8b1177f5b28ff4833052ee868d0e477e39466283
chris-wamae <chriswamae123@gmail.com>
2023-06-19 07:17:24 +0300

    Refactored area methods to accept an array as parameters

--- calculations/Area.cs
+++ calculations/Area.cs
@@ -1,27 +1,39 @@
 public class Area
 {
-    public static double Square(double side)
+    public static double Square(params double[] sides)
     {
-        return side * side;
+        return sides[0] * sides[0];
     }
 
-    public static double Rectangle(double length, double width)
+    public static double Rectangle(params double[] sides)
     {
+        double length = sides[0];
+        double width = sides[1];
+
         return length * width;
     }
 
-    public static double Circle(double radius)
+    public static double Circle(params double[] radius)
     {
-        return Math.PI * radius * radius;
+        return Math.PI * radius[0] * radius[0];
     }
 
-    public static double Triangle_right_angled(double triagle_base, double straight_height){
+    public static double Triangle_right_angled(params double[] sides)
+    {
+
+        double triagle_base = sides[0];
+
+        double straight_height = sides[1];
 
         return 0.5 * triagle_base * straight_height;
 
     }
 
-    public static double Triangle_isosceles_slant(double triangle_base, double slant_height){
+    public static double Triangle_isosceles_slant(params double[] sides)
+    {
+
+        double triangle_base = sides[0];
+        double slant_height = sides[1];
 
         double straight_height = Calculation_helper.Triangle_height(slant_height, triangle_base * 0.5);
 
@@ -30,8 +42,12 @@ public class Area
 
 
 
-    public static double Trapezium_slant(double top, double bottom, double slant_height)
+    public static double Trapezium_slant(params double[] sides)
     {
+        double top = sides[0];
+        double bottom = sides[1];
+        double slant_height = sides[2];
+
         double difference = Math.Sqrt((top - bottom) * (top - bottom));
 
         double triangle_base = difference / 2;
@@ -42,10 +58,14 @@ public class Area
 
     }
 
-    public static double Trapezium_straight(double top, double bottom, double straight_height){
+    public static double Trapezium_straight(params double[] sides)
+    {
+        double top = sides[0];
+        double bottom = sides[1];
+        double straight_height = sides[2];
 
-       return (top + bottom) * 0.5 * straight_height;
+        return (top + bottom) * 0.5 * straight_height;
 
-    } 
+    }
 
 }
